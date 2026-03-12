9afc9a4007b2f62f4740cf274467a7b1e5d0e7b1
rubyjinx3-sudo <rubyjinx3@gmail.com>
2026-03-12 20:35:48 +0800

    Fix Gradle version compatibility - mobile

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.campuseventtracker"
-    compileSdk {
-        version = release(35) {
-
-        }
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.campuseventtracker"
